83693e03920afe7b544bb607607a50566ab27030
davidcasr <david.castroruiz@gmail.com>
2020-03-05 16:59:48 -0500

    Install Laravel Debugbar

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // Laravel Debugbar
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -229,6 +231,8 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
+        // Laravel Debugbar
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 

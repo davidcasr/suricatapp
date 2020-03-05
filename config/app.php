c596414da39bb02b89bb94b2ade9c46bb711c030
davidcasr <david.castroruiz@gmail.com>
2020-03-05 16:39:12 -0500

    Install and configure laravel-generator

--- config/app.php
+++ config/app.php
@@ -225,6 +225,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // Laravel Generator 
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 

9372631c6caff6aa67a7baa89c0d8700f17f4c6a
davidcasr <david.castroruiz@gmail.com>
2020-03-16 16:36:47 -0500

    [Authentication] Update of authentication redirect by type user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -37,4 +39,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function redirectPath(){
+
+        if(Auth::user()->isAn('admin')){
+           return RouteServiceProvider::HOME;
+        }elseif(Auth::user()->isAn('super')){
+            return RouteServiceProvider::HOME_ADMIN;
+        }
+        
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['auth','can:admin_all']], function () {
 	Route::resource('dashboard', 'DashboardController');
 	Route::resource('communities', 'CommunityController');
 	Route::resource('people', 'PersonController');
@@ -30,17 +30,16 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::resource('meeting_reports', 'MeetingReportController');
 	Route::resource('assistants', 'AssistantController');
 
-
-	Route::group(['middleware' => 'can:super_all'], function () {
-		// Features for the administrator 
-		Route::resource('admin/admin_dashboard', 'Administrator\\DashboardController');
-		Route::resource('admin/users', 'Administrator\\UserController');
-		Route::resource('admin/roles', 'Administrator\\RoleController');
-		Route::resource('admin/abilities', 'Administrator\\AbilityController');
-		Route::resource('admin/gen_groups', 'Administrator\\GenGroupController');
-		Route::resource('admin/gen_lists', 'Administrator\\GenListController');
-		Route::resource('admin/plans', 'Administrator\\PlanController');
-		Route::resource('admin/plan_users', 'Administrator\\PlanUserController');
-	});	
-
 });
+
+Route::group(['middleware' => ['auth','can:super_all']], function () {
+	// Features for the administrator 
+	Route::resource('admin/admin_dashboard', 'Administrator\\DashboardController');
+	Route::resource('admin/users', 'Administrator\\UserController');
+	Route::resource('admin/roles', 'Administrator\\RoleController');
+	Route::resource('admin/abilities', 'Administrator\\AbilityController');
+	Route::resource('admin/gen_groups', 'Administrator\\GenGroupController');
+	Route::resource('admin/gen_lists', 'Administrator\\GenListController');
+	Route::resource('admin/plans', 'Administrator\\PlanController');
+	Route::resource('admin/plan_users', 'Administrator\\PlanUserController');
+});	

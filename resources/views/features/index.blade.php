11922d588ac7134d0479b54c1e0646256d901811
davidcasr <david.castroruiz@gmail.com>
2020-03-24 11:16:59 -0500

    I updated icon in the index of some views.

--- resources/views/features/index.blade.php
+++ resources/views/features/index.blade.php
@@ -5,7 +5,7 @@
         <div class="page-title-wrapper">
             <div class="page-title-heading">
                 <div class="page-title-icon">
-                    <i class="pe-7s-news-paper icon-gradient bg-mean-fruit">
+                    <i class="pe-7s-user icon-gradient bg-mean-fruit">
                     </i>
                 </div>
                 <div>@choice('functionalities.features', 1)
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,7 +5,7 @@
         <div class="page-title-wrapper">
             <div class="page-title-heading">
                 <div class="page-title-icon">
-                    <i class="pe-7s-note icon-gradient bg-mean-fruit">
+                    <i class="pe-7s-graph1 icon-gradient bg-mean-fruit">
                     </i>
                 </div>
                 <div>@choice('functionalities.profiles', 1)
